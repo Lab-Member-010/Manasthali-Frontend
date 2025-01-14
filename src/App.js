af5b5c24308d099faeb1ae8143054a6c5bd43a72
archana <archanameena9630@gmail.com>
2025-01-14 10:14:19 +0530

    random updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 
 import React from 'react';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App</h1>
-        <p>This is a simple React application.</p>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
