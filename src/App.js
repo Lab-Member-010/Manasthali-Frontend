991cd6fc4fa483099e45429669956e21a282cb1b
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-15 23:06:14 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -23,45 +23,45 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Signin from "./components/Signin";
 
-import ForgetPassword from "./components/ForgetPassword";
-import ResetPassword from "./components/ResetPassword";
+// import ForgetPassword from "./components/ForgetPassword";
+// import ResetPassword from "./components/ResetPassword";
 // import SignUp from "./components/Signup";
 
-const App = () => {
-  return (
-   
-    <Router>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/forgot-password" element={<ForgetPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </Router>
-  
-  );
-};
-
-export default App;
-
-// import React from "react";
-// import { useSearchParams } from "react-router-dom";
-// import Chat from "./components/Chat";
-
-// function App() {
-//   const [searchParams] = useSearchParams();
-//   const token = searchParams.get("token");
-//   const userId = searchParams.get("userId");
-
-//   if (!token || !userId) {
-//     return <p>Please provide token and userId as query parameters in the URL.</p>;
-//   }
-
+// const App = () => {
 //   return (
-//     <div>
-//       <h1>Chat Application</h1>
-//       <Chat user={{ _id: userId }} token={token} />
-//     </div>
+   
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Signin />} />
+//         <Route path="/forgot-password" element={<ForgetPassword />} />
+//         <Route path="/reset-password" element={<ResetPassword />} />
+//       </Routes>
+//     </Router>
+  
 //   );
-// }
+// };
 
 // export default App;
+
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import Chat from "./components/Chat";
+
+function App() {
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
+  const userId = searchParams.get("userId");
+
+  if (!token || !userId) {
+    return <p>Please provide token and userId as query parameters in the URL.</p>;
+  }
+
+  return (
+    <div>
+      <h1>Chat Application</h1>
+      <Chat user={{ _id: userId }} token={token} />
+    </div>
+  );
+}
+
+export default App;
