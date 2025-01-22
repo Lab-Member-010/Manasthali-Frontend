2308eb5f8575b1e9983f52ed73599a6a3f763282
archana <archanameena9630@gmail.com>
2025-01-22 10:42:42 +0530

    random

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import SignUp from "./components/Authentication/Signup";
+import Signin from "./components/Authentication/Signin";
+import Verifyotp from "./components/Authentication/Verifyotp";
+import ForgetPassword from "./components/Authentication/ForgetPassword";
+import ResetPassword from "./components/Authentication/ResetPassword";
+import QuizGetStarted from "./components/Quiz/QuizGetStarted";
+import Quiz from "./components/Quiz/Quiz";
+import Feed from "./components/Feed/Feed";
+import Auth from "./components/Authorization/Auth";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Signup Page */}
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* OTP Verification Page */}
+        <Route path="/verify-otp" element={<Verifyotp />} />
+
+        {/* Sign-in Page */}
+        <Route path="/signin" element={<Signin />} />
+
+        {/* Forgot Password Page */}
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+
+        {/* Reset Password Page */}
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* Quiz Get Started Page */}
+        <Route path="/quiz-start" element={<Auth><QuizGetStarted /></Auth>} />
+
+        {/* Quiz Page */}
+        <Route path="/quiz" element={<Auth><Quiz /></Auth>} />
+
+        {/* Feed Page */}
+        <Route path="/feed" element={<Auth><Feed /></Auth>} />
+      </Routes>
+       
+       {/* Messages Page */}
+       {/* <Route path="/message" element={<Auth><Message /></Auth>} */}
+      {/* <div className="App">
+      <ChatApp/>
+    </div> */}
+    </>
+  );
+};
+export default App;
