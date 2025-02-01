98dc6bdf661a9a1090358a4ffda8988f2a8d53ff
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-02-01 21:54:51 +0530

    Admin Added

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,11 @@
+import styles from "./Admin.module.css";
+
+const Admin=()=>{
+    return(
+        <div className={styles.adminController}>
+            Admin Controller
+        </div>
+    );
+};
+
+export default Admin;
