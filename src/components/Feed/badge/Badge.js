d42827f748f8cdc6de6415ffe4423225b808b40a
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-02-03 12:52:41 +0530

    group and groupchat terminate

--- src/components/Feed/badge/Badge.js
+++ src/components/Feed/badge/Badge.js
@@ -1,7 +1,6 @@
 import construction from "../../../images/Under-Construction.gif";
 
 const Badge = () => {
-  
   return (
     <div className="container-fluid d-flex justify-content-center">
       <img src={construction} alt="under construction"/>
